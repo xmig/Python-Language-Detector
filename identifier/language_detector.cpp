a9e890d6287f11b8fea9b9373ed75d4185edd562
Sergii Tretiak <stretyak@google.com>
2018-01-17 18:06:09 +0200

    Python 3.x compatible - NOT cheked yet

--- identifier/language_detector.cpp
+++ identifier/language_detector.cpp
@@ -1,5 +1,7 @@
 // YAY?!?!
 
+#include <cstdlib>
+#include<string.h>
 #include <Python.h>
 #include "language_sample.h"
 #include "language_database.h"
@@ -16,16 +18,6 @@ static PyMethodDef LangMethods[] = {
     {NULL, NULL, 0, NULL}
 };
 
-PyMODINIT_FUNC initlanguageIdentifier(void) {
-	PyObject *m;
-	m = Py_InitModule("languageIdentifier", LangMethods);
-	if (m == NULL)
-		return;
-		
-	LangError = PyErr_NewException("languageIdentifier.error", NULL, NULL);
-    Py_INCREF(LangError);
-    PyModule_AddObject(m, "error", LangError);
-}
 
 PyObject *lang_identify(PyObject *self, PyObject *args) {
 	int maxTrigrams = 300, maxInputLength = 900;
@@ -55,15 +47,61 @@ PyObject *lang_loadtrigrams(PyObject *self, PyObject *args) {
 	return Py_BuildValue("s", pathName);
 }
 
-int main (int argc, char **argv) {
+/* char to wchar_t */
+wchar_t * ctow(const char* c, size_t max)
+{
+	wchar_t * w = new wchar_t[max];
+	mbstowcs(w,c,max);
+	return w;
+}
+
+inline void wfree( wchar_t *buff ) { delete [] buff; }
+
+
+//PyMODINIT_FUNC initlanguageIdentifier(void) {
+//	PyObject *m;
+//	m = Py_InitModule("languageIdentifier", LangMethods);
+//	if (m == NULL)
+//		return;
+//
+//	LangError = PyErr_NewException("languageIdentifier.error", NULL, NULL);
+//    Py_INCREF(LangError);
+//    PyModule_AddObject(m, "error", LangError);
+//}
+
+static struct PyModuleDef languageIdentifier =
+{
+        PyModuleDef_HEAD_INIT,
+        "languageIdentifier", /* name of module */
+        "",          /* module documentation, may be NULL */
+        -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+        LangMethods
+};
+
+PyMODINIT_FUNC PyInit_languageIdentifier(void)
+{
+    return PyModule_Create(&languageIdentifier);
+}
+
+int main (int argc, char ** argv) {
     /* Pass argv[0] to the Python interpreter */
-    Py_SetProgramName(argv[0]);
+    //Py_SetProgramName(argv[0]);
+
+	size_t len = strlen(argv[0]);
+
+	wchar_t* name = ctow(argv[0], len );
+	name[len+1] = '\0';
+
+    Py_SetProgramName(name);
+	//wfree(name);
+
 
     /* Initialize the Python interpreter.  Required. */
     Py_Initialize();
 
     /* Add a static module */
-    initlanguageIdentifier();
+    PyInit_languageIdentifier();
+    //initlanguageIdentifier();
 }
 
 
